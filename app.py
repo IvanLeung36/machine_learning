7c58a45fb0abc1c7f954ec3cea9589f54b4acfd0
IvanLeung36 <ivanleung36@gmail.com>
2025-03-30 21:50:55 +0100

    fix port

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify, render_template
 import joblib
 import logging
+import os
+
 
 app = Flask(__name__)
 
@@ -69,4 +71,5 @@ def predict():
         return jsonify({'error': 'An error occurred during prediction'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
